458ed1d9b5506cfb05a6db3433e0413a7b468709
Maxence <maxence.delacroix@epitech.eu>
2019-07-12 01:26:03 +0200

    good part of project

---
+++ WesternSaloonAdam/Assets/Timeline/MyTimeline.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class MyTimeline : MonoBehaviour
+{
+    PlayableDirector playableDirector;
+
+    void Start()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+}
